d2d16df930a545aaecc48de586f2ec4e114cd2e2
AKIRA-Auth <blueunicorn0805@gmail.com>
2024-10-02 00:53:34 +0700

    create : deploy_view_

---
+++ scripts/deploy_view_pure.js
@@ -0,0 +1,26 @@
+// Import ethers from hardhat
+const { ethers } = require("hardhat");
+
+async function main() {
+  // Get the contract factory and deployer account
+  const [deployer] = await ethers.getSigners();
+  
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  // Check deployer's balance
+  const balance = await deployer.getBalance();
+  console.log("Account balance:", ethers.utils.formatEther(balance));
+
+  // Deploy the ViewAndPure contract
+  const ViewAndPureContract = await ethers.getContractFactory("ViewAndPure");
+  const viewAndPureInstance = await ViewAndPureContract.deploy();
+  await viewAndPureInstance.deployed();
+  console.log("ViewAndPure contract deployed at:", viewAndPureInstance.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
