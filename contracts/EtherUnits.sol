b995f6a9266f83263f7ec828ede4edb47abf1cb5
AKIRA-Auth <blueunicorn0805@gmail.com>
2024-10-01 17:12:22 +0700

    create : Ether

---
+++ contracts/EtherUnits.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract EtherUnits {
+    uint256 public oneWei = 1 wei;
+    // 1 wei is equal to 1
+    bool public isOneWei = (oneWei == 1);
+
+    uint256 public oneGwei = 1 gwei;
+    // 1 gwei is equal to 10^9 wei
+    bool public isOneGwei = (oneGwei == 1e9);
+
+    uint256 public oneEther = 1 ether;
+    // 1 ether is equal to 10^18 wei
+    bool public isOneEther = (oneEther == 1e18);
+}
